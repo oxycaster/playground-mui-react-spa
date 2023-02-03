69bb4c05c269d4356d1c733a94530ee07b204a84
oxycaster <480125+oxycaster@users.noreply.github.com>
2023-02-04 01:47:00 +0900

    MUIが使えるようになりました

--- src/main/routes/Root.tsx
+++ src/main/routes/Root.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import {Button, Container, Grid} from "@mui/material";
 
 export const Root = () => {
     return (
-        <div>Hello world!!!!</div>
+        <Container maxWidth="sm">
+            <Grid container spacing={2}>
+                <Grid item xs={8}>
+                    <div>Hello world!!!!</div>
+                </Grid>
+                <Grid item xs={4}>
+                    <Button variant="text">Text</Button>
+                </Grid>
+                <Grid item xs={4}>
+                    <Button variant="contained">Contained</Button>
+                </Grid>
+                <Grid item xs={8}>
+                    <Button variant="outlined">Outlined</Button>
+                </Grid>
+            </Grid>
+        </Container>
     );
 };
